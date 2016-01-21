f81077b227e3b22c9d27bc94ef7372f241a75490
Nick Owens <mischief@offblast.org>
2016-01-20 21:27:34 -0800

    init

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/proxy"
+	goproxy "gopkg.in/elazarl/goproxy.v1"
+)
+
+var (
+	listenAddr = flag.String("listen", "127.0.0.1:8118", "http proxy listen address")
+	socksAddr  = flag.String("socks", "127.0.0.1:9050", "socks5 server address")
+	verbose    = flag.Bool("v", false, "verbose")
+)
+
+func main() {
+	logger := log.New(os.Stderr, "http2socks: ", log.LstdFlags|log.Lshortfile)
+
+	flag.Parse()
+
+	prxy := goproxy.NewProxyHttpServer()
+	prxy.Logger = logger
+	prxy.Verbose = *verbose
+
+	dialer, err := proxy.SOCKS5("tcp", *socksAddr, nil, proxy.Direct)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	prxy.ConnectDial = dialer.Dial
+
+	logger.Fatal(http.ListenAndServe(*listenAddr, prxy))
+}
